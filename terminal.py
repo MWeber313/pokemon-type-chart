c2b7bc185ddd2ede464fe07c0cf4210bf49241ae
Jeremy Brady <jbdigitalgeek@gmail.com>
2020-07-08 19:16:34 -0700

    more tinkering/case handling frames in

--- terminal.py
+++ terminal.py
@@ -1,13 +1,28 @@
-import click
+# import click
+import re
+choice = input("Attack or Defend? \n").lower()
 
-file = open('types.txt', 'r')
-types = file.readlines().split('\n')
+atkFile = open("attack_types.txt", "r")
+atkTypes = atkFile.read().split('\n\n')
+# cleanAtk = atkTypes.replace('\n', ',')
+atkFile.close()
+print("atkfile", atkFile)
+defFile = open("defend_types.txt", "r")
+defTypes = defFile.read().split("\n\n")
 # cleanTypes = types.split(': ')
-file.close()
+defFile.close()
+for lines in atkTypes:
+    newLines=lines.split("\n")
+test = []
+if choice == "defend":
+    print("defend", defTypes)
+elif choice == "attack":
+    print("attack", atkTypes)
 
-# for lines in types:
-#     print("l",lines)
-
-print(types)
+    # test.append(lines)
+    # print("atklines", lines[0:])
+# types = str(types).split('\\n')
+print("test", test)
+# print('dicttest', lineDict)
 # @click.command()
-
+# if end of line == : then go to next line with end of line == : and make object
