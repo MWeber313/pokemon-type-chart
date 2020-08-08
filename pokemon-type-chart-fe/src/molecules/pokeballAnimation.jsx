69c2ca7afd554a98eeed4fdb793da4c45956a72f
melissa-murphy <melissamurphy.ca@gmail.com>
2020-08-07 20:03:30 -0700

    form functional, unstyled. need if/else for zindex

--- pokemon-type-chart-fe/src/molecules/pokeballAnimation.jsx
+++ pokemon-type-chart-fe/src/molecules/pokeballAnimation.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { useForm } from "react-hook-form";
 import FluidImg from "../atoms/fluidImg";
 import Container from "../atoms/container";
 // images
@@ -7,13 +8,28 @@ import pokeTop from "../img/pokeballtop.png";
 import pokeBottom from "../img/pokeballbottom.png";
 
 const PokeballAnimation = () => {
+	const { register, handleSubmit, watch, errors, getValues } = useForm();
+	const data = getValues();
+	const onSubmit = (data) => console.log(data);
+
+	console.log(`Watching form input:` + watch);
 	return (
 		<>
 			<AnimationWrapper>
 				<Container height={20}>
 					<FluidImg src={pokeTop} />
 				</Container>
-                {/* last-child top=150px ? show form : don't show form */}
+				{/* last-child top=150px ? show form (z-index) : don't show form */}
+				<Container margin={"-10rem 0"}>
+					<form onSubmit={handleSubmit(onSubmit)}>
+						<input
+							name="searchName"
+							defaultValue="Search for Pokemon"
+							ref={register}
+						/>
+						<input type="submit" />
+					</form>
+				</Container>
 				<Container height={20} position={"absolute"}>
 					<FluidImg src={pokeBottom} />
 				</Container>
