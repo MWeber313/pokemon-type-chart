6a5cdae2141ff573f28e93b08ebc8a8bd16405d5
Melissa Murphy <melissa@thoughtfulmonkey.design>
2020-08-12 15:58:58 -0700

    form visibility. but when click on form, hides

--- pokemon-type-chart-fe/src/molecules/pokeballAnimation.jsx
+++ pokemon-type-chart-fe/src/molecules/pokeballAnimation.jsx
@@ -1,62 +1,68 @@
-import React, { useState, useEffect } from "react";
-import styled from "styled-components";
-import { useForm } from "react-hook-form";
-import FluidImg from "../atoms/fluidImg";
-import Container from "../atoms/container";
+import React, { useState, useEffect } from "react"
+import styled from "styled-components"
+import { useForm } from "react-hook-form"
+import { useSpring, animated } from "react-spring"
+import FluidImg from "../atoms/fluidImg"
+import Container from "../atoms/container"
 // images
-import pokeTop from "../img/pokeballtop.png";
-import pokeBottom from "../img/pokeballbottom.png";
+import pokeTop from "../img/pokeballtop.png"
+import pokeBottom from "../img/pokeballbottom.png"
 
 const PokeballAnimation = () => {
-	const { register, handleSubmit, watch, errors, getValues } = useForm();
-	const onSubmit = () => console.log(getValues());
-	const [isBallOpen, setIsBallOpen] = useState(false);
-	console.log(`Watching form input:` + watch);
-	console.log(isBallOpen);
-	return (
-		<>
-			<AnimationWrapper
-				isBallOpen={isBallOpen}
-				onToggleBall={() => setIsBallOpen(!isBallOpen)}
-			>
-				<Container height={40}>
-					<FluidImg src={pokeTop} />
-				</Container>
-				{/* last-child top=150px ? show form (z-index) : don't show form */}
-				{/*  */}
-				<Container margin={"-12vh 0"}>
-					<form id="pokeForm" onSubmit={handleSubmit(onSubmit)}>
-						<input
-							name="searchName"
-							defaultValue="Search for Pokemon"
-							ref={register}
-						/>
-						<input type="submit" />
-					</form>
-				</Container>
-				<Container
-					id="pokeballBottom"
-					height={40}
-					position={"absolute"}
-					top={"0"}
-					left={"0"}
-				>
-					<FluidImg src={pokeBottom} />
-				</Container>
-			</AnimationWrapper>
-		</>
-	);
-};
+  const { register, handleSubmit, watch, errors, getValues } = useForm()
+  const onSubmit = () => console.log(getValues())
+  const [isBallOpen, setIsBallOpen] = useState(false)
 
-const AnimationWrapper = styled.div`
-	position: relative;
-	/* div:last-child {
-		transition: all 1s;
-		&:hover {
-			top: 150px;
-			transition: all 1s;
-		} */
-	}
-`;
+  useEffect(() => {
+    // change ball
+    console.log(`isBallOpen: ${isBallOpen}`)
+  })
 
-export default PokeballAnimation;
+  const ShowForm = ({ isBallOpen }) => {
+    const props = useSpring({
+      opacity: isBallOpen ? 1 : 0,
+      from: { opacity: 0 },
+      zIndex: isBallOpen ? 999 : 0,
+    })
+    return (
+      <animated.div style={props}>
+        <form id="pokeForm" onSubmit={handleSubmit(onSubmit)}>
+          <input
+            name="searchName"
+            defaultValue="Search for Pokemon"
+            ref={register}
+          />
+          <input type="submit" />
+        </form>
+      </animated.div>
+    )
+  }
+
+  //   console.log(`Watching form input:` + watch)
+  return (
+    <>
+      <AnimationWrapper
+        className={"animationWrapper"}
+        isBallOpen={isBallOpen}
+        onClick={() => setIsBallOpen(!isBallOpen)}>
+        <Container height={40}>
+          <FluidImg src={pokeTop} />
+        </Container>
+        <Container margin={"-12vh 0"}>
+          <ShowForm isBallOpen={isBallOpen} />
+        </Container>
+        <Container
+          id="pokeballBottom"
+          height={40}
+          position={"absolute"}
+          top={"20vh"}
+          left={"0"}>
+          <FluidImg src={pokeBottom} />
+        </Container>
+      </AnimationWrapper>
+    </>
+  )
+}
+
+const AnimationWrapper = styled.div``
+export default PokeballAnimation
