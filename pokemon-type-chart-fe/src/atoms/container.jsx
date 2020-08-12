958460dfb370972ce06bda9a68c0434fa9f28e95
Melissa Murphy <melissa@thoughtfulmonkey.design>
2020-08-12 16:18:55 -0700

    react spring w/ useEffect needed

--- pokemon-type-chart-fe/src/atoms/container.jsx
+++ pokemon-type-chart-fe/src/atoms/container.jsx
@@ -1,78 +1,81 @@
-import React from "react";
-import styled from "styled-components";
-import colors from "../styles/colors";
+import React from "react"
+import styled from "styled-components"
+import colors from "../styles/colors"
 
 const Container = ({ ...props }) => {
-	const {
-		height,
-		width,
-		display,
-		flexDirection,
-		alignSelf,
-		textAlign,
-		backgroundColor,
-		padding,
-		margin,
-		position,
-		top,
-		bottom,
-		left,
-	} = props;
-	return (
-		<>
-			<StyledContainer
-				className="container"
-				height={height}
-				width={width}
-				display={display}
-				flexDirection={flexDirection}
-				alignSelf={alignSelf}
-				textAlign={textAlign}
-				backgroundColor={backgroundColor}
-				padding={padding}
-				margin={margin}
-				position={position}
-				top={top}
-				bottom={bottom}
-				left={left}
-			>
-				{props.children}
-			</StyledContainer>
-		</>
-	);
-};
+  const {
+    height,
+    width,
+    display,
+    flexDirection,
+    alignSelf,
+    textAlign,
+    backgroundColor,
+    padding,
+    margin,
+    position,
+    top,
+    bottom,
+    left,
+    zIndex,
+  } = props
+  return (
+    <>
+      <StyledContainer
+        className="container"
+        height={height}
+        width={width}
+        display={display}
+        flexDirection={flexDirection}
+        alignSelf={alignSelf}
+        textAlign={textAlign}
+        backgroundColor={backgroundColor}
+        padding={padding}
+        margin={margin}
+        position={position}
+        top={top}
+        bottom={bottom}
+        left={left}
+        zIndex={zIndex}>
+        {props.children}
+      </StyledContainer>
+    </>
+  )
+}
 
 const StyledContainer = styled.div.attrs((props) => ({
-	height: props.height,
-	width: props.width,
-	textAlign: props.textAlign,
-	display: props.display,
-	flexDirection: props.flexDirection,
-	alignSelf: props.alignSelf,
-	backgroundColor: props.backgroundColor,
-	padding: props.padding,
-	margin: props.margin,
-	top: props.top,
-	bottom: props.bottom,
-	left: props.left,
+  height: props.height,
+  width: props.width,
+  textAlign: props.textAlign,
+  display: props.display,
+  flexDirection: props.flexDirection,
+  alignSelf: props.alignSelf,
+  backgroundColor: props.backgroundColor,
+  padding: props.padding,
+  margin: props.margin,
+  top: props.top,
+  bottom: props.bottom,
+  left: props.left,
+  zIndex: props.zIndex,
 }))`
-	height: ${(props) => (props.height ? `${props.height}vh` : "auto")};
-	width: ${(props) => (props.width ? `${props.width}vw` : "100vw")};
-	text-align: ${(props) => (props.textAlign ? props.textAlign : "center")};
-	display: flex;
-	flex-direction: ${(props) =>
-		props.flexDirection ? props.flexDirection : "row"};
-	align-self: ${(props) => (props.alignSelf ? props.alignSelf : null)};
-	justify-content: center;
-	align-items: center;
-	background-color: ${(props) =>
-		props.backgroundColor ? props.backgroundColor : colors.white};
-	padding: ${(props) => (props.padding ? props.padding : null)};
-	margin: ${(props) => (props.margin ? props.margin : null)};
-	position: ${(props) => (props.position ? props.position : "relative")};
-	top: ${(props) => (props.top ? props.top : "auto")};
-	bottom: ${(props) => (props.bottom ? props.bottom : "auto")};
-	left: ${(props) => (props.left ? props.left : "auto")};
-`;
+  height: ${(props) => (props.height ? `${props.height}vh` : "auto")};
+  width: ${(props) => (props.width ? `${props.width}vw` : "100vw")};
+  text-align: ${(props) => (props.textAlign ? props.textAlign : "center")};
+  display: flex;
+  flex-direction: ${(props) =>
+    props.flexDirection ? props.flexDirection : "row"};
+  align-self: ${(props) => (props.alignSelf ? props.alignSelf : null)};
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) =>
+    props.backgroundColor ? props.backgroundColor : colors.white};
+  padding: ${(props) => (props.padding ? props.padding : null)};
+  margin: ${(props) => (props.margin ? props.margin : null)};
+  position: ${(props) => (props.position ? props.position : "relative")};
+  top: ${(props) => (props.top ? props.top : "auto")};
+  bottom: ${(props) => (props.bottom ? props.bottom : "auto")};
+  left: ${(props) => (props.left ? props.left : "auto")};
+  z-index: ${(props) => (props.zIndex ? props.zIndex : "auto")};
+`
 
-export default Container;
+export default Container
