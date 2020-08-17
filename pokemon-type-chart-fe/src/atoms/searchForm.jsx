53d2f569b2a1c87ca3b21870120fb6b87757923a
melissa-murphy <melissamurphy.ca@gmail.com>
2020-08-17 13:31:30 -0700

    functional form data, autofocus

---
+++ pokemon-type-chart-fe/src/atoms/searchForm.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+
+const defaultValues = {
+	Search: "",
+};
+
+const SearchForm = () => {
+	const { handleSubmit, register } = useForm({ defaultValues });
+	const [data, setData] = useState(null);
+	let renderCount = 0;
+	renderCount++;
+	console.log(`Render Count: ${renderCount} Data: ${JSON.stringify(data)}`);
+	return (
+		<>
+			<form style={{display: "flex", flexDirection: 'column'}} onSubmit={handleSubmit((data) => setData(data))}>
+				<label value="Search Pokedex">Search Pokedex</label>
+				<input autoFocus type="text" name="Search" ref={register} />
+				{/* add onClick reset(defaultValues)? */}
+				<input type="submit" value="submit" />
+			</form>
+		</>
+	);
+};
+
+export default SearchForm;
--- pokemon-type-chart-fe/src/molecules/pokeballAnimation.jsx
+++ pokemon-type-chart-fe/src/molecules/pokeballAnimation.jsx
@@ -1,16 +1,14 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import { useForm } from "react-hook-form";
 import { useSpring, animated } from "react-spring";
 import FluidImg from "../atoms/fluidImg";
 import Container from "../atoms/container";
 // images
 import pokeTop from "../img/pokeballtop.png";
 import pokeBottom from "../img/pokeballbottom.png";
+import SearchForm from "../atoms/searchForm";
 
 const PokeballAnimation = () => {
-	const { register, handleSubmit, watch, errors, getValues } = useForm();
-	const onSubmit = () => console.log(getValues());
 	const [isBallOpen, setIsBallOpen] = useState(false);
 
 	useEffect(() => {
@@ -21,30 +19,22 @@ const PokeballAnimation = () => {
 
 	const ShowForm = ({ isBallOpen }) => {
 		const props = useSpring({
-			marginTop: !isBallOpen ? "-1000px" : "-200px",
+			marginTop: !isBallOpen ? "-1000px" : "-225px",
 			from: { marginTop: "-1000px" },
 			display: "flex",
       justifyContent: "space-around",
       overflowY: "hidden"
 		});
-		console.log(errors, watch);
 		return (
 			<animated.div style={props}>
-				<form id="pokeForm" onSubmit={handleSubmit(onSubmit)}>
-					<input
-						name="searchName"
-						defaultValue="Search for Pokemon"
-						ref={register}
-					/>
-					<input type="submit" />
-				</form>
+        <SearchForm />
 			</animated.div>
 		);
 	};
 
 	const OpenBall = ({ isBallOpen }) => {
 		const props = useSpring({
-			marginTop: !isBallOpen ? "-45px" : "100px",
+			marginTop: !isBallOpen ? "-41px" : "100px",
 			from: { marginTop: "0px" },
 			// zIndex: 999,
 		});
