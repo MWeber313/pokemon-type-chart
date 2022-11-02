209d006d7fb239f0b708654e25ff64d65c6c8b71
MWeberLambdaweb19 <mack.webb37@gmail.com>
2022-11-01 17:56:39 -0700

    Modified into class, currently hammering out bugs

--- pokemon.py
+++ pokemon.py
@@ -1,32 +1,30 @@
+from contextlib import nullcontext
 import requests
 
-
 cache = dict()
+pokeTYPE = None 
 
+class Pokemon:
+    def show_cache(self, cache):
+        for i in cache:
+            print(i)
+        
+    def set_type(self, type):
+        pokeTYPE = type
+        print(pokeTYPE)
+    def get_article_from_server(url):
+        response = requests.get(url)
+        return response.text
 
-def get_article_from_server(url):
-
-    print("Fetching article from server...")
-
-    response = requests.get(url)
-
-    return response.text
-
-
-def get_article(url):
-
-    print("Getting article...")
-
-    if url not in cache:
-
-        cache[url] = get_article_from_server(url)
-
-
-    return cache[url]
+    def get_article(self, url):
+        if url not in cache:
+            cache[url] = self.get_article_from_server(url)
+        return cache[url]
 
 
 print("Select Pokemon Type")
 pokeType = input()
-get_article(f"https://pokeapi.co/api/v2/type/{pokeType}")
-
+pokeURL = f"https://pokeapi.co/api/v2/type/{pokeType}"
+programInstance = Pokemon()
+programInstance.get_article(pokeURL)
 print(cache)
