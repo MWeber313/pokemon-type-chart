92a554a585f01d69b54371a1f9ea8f48b6588d8c
MWeberLambdaweb19 <mack.webb37@gmail.com>
2022-11-02 18:37:09 -0700

    Updated so method Set Type works

--- pokemon.py
+++ pokemon.py
@@ -4,27 +4,39 @@ import requests
 cache = dict()
 pokeTYPE = None 
 
-class Pokemon:
-    def show_cache(self, cache):
+class Pokemon():
+    def __init__(self) -> None:
+        super().__init__()
+        self.cache = dict()
+        self.pokeTYPE = None
+        
+    def show_cache(self):
+        cache = self.cache
         for i in cache:
             print(i)
         
+        return 
     def set_type(self, type):
         pokeTYPE = type
         print(pokeTYPE)
-    def get_article_from_server(url):
-        response = requests.get(url)
-        return response.text
+
+        return pokeTYPE
+
+    def get_article_from_server(self, url):
+        self.cache.update({pokeTYPE:url})
+        return 
+        # response = requests.get(url)
+        # return response.text
 
     def get_article(self, url):
-        if url not in cache:
-            cache[url] = self.get_article_from_server(url)
-        return cache[url]
+
+        return
 
 
 print("Select Pokemon Type")
 pokeType = input()
-pokeURL = f"https://pokeapi.co/api/v2/type/{pokeType}"
+# pokeURL = f"https://pokeapi.co/api/v2/type/{pokeType}"
 programInstance = Pokemon()
-programInstance.get_article(pokeURL)
-print(cache)
+programInstance.set_type(pokeType)
+programInstance.get_article_from_server(pokeTYPE)
+programInstance.show_cache()
