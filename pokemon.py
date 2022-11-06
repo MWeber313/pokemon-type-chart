b34c392f5b9da4be9686b74d096b7edc6f84b3e1
MWeberLambdaweb19 <mack.webb37@gmail.com>
2022-11-05 20:09:02 -0700

    using pokemon.py as scratch, will rename. type_getter is new program

--- pokemon.py
+++ pokemon.py
@@ -4,87 +4,87 @@ cache = dict()
 pokeTYPE = None 
 
 
-response = {"damage_relations": {
-		"double_damage_from": [
-			{
-				"name": "flying",
-				"url": "https://pokeapi.co/api/v2/type/3/"
-			},
-			{
-				"name": "psychic",
-				"url": "https://pokeapi.co/api/v2/type/14/"
-			},
-			{
-				"name": "fairy",
-				"url": "https://pokeapi.co/api/v2/type/18/"
-			}
-		],
-		"double_damage_to": [
-			{
-				"name": "normal",
-				"url": "https://pokeapi.co/api/v2/type/1/"
-			},
-			{
-				"name": "rock",
-				"url": "https://pokeapi.co/api/v2/type/6/"
-			},
-			{
-				"name": "steel",
-				"url": "https://pokeapi.co/api/v2/type/9/"
-			},
-			{
-				"name": "ice",
-				"url": "https://pokeapi.co/api/v2/type/15/"
-			},
-			{
-				"name": "dark",
-				"url": "https://pokeapi.co/api/v2/type/17/"
-			}
-		],
-		"half_damage_from": [
-			{
-				"name": "rock",
-				"url": "https://pokeapi.co/api/v2/type/6/"
-			},
-			{
-				"name": "bug",
-				"url": "https://pokeapi.co/api/v2/type/7/"
-			},
-			{
-				"name": "dark",
-				"url": "https://pokeapi.co/api/v2/type/17/"
-			}
-		],
-		"half_damage_to": [
-			{
-				"name": "flying",
-				"url": "https://pokeapi.co/api/v2/type/3/"
-			},
-			{
-				"name": "poison",
-				"url": "https://pokeapi.co/api/v2/type/4/"
-			},
-			{
-				"name": "bug",
-				"url": "https://pokeapi.co/api/v2/type/7/"
-			},
-			{
-				"name": "psychic",
-				"url": "https://pokeapi.co/api/v2/type/14/"
-			},
-			{
-				"name": "fairy",
-				"url": "https://pokeapi.co/api/v2/type/18/"
-			}
-		],
-		"no_damage_from": [],
-		"no_damage_to": [
-			{
-				"name": "ghost",
-				"url": "https://pokeapi.co/api/v2/type/8/"
-			}
-		]
-	},}
+# response = {"damage_relations": {
+# 		"double_damage_from": [
+# 			{
+# 				"name": "flying",
+# 				"url": "https://pokeapi.co/api/v2/type/3/"
+# 			},
+# 			{
+# 				"name": "psychic",
+# 				"url": "https://pokeapi.co/api/v2/type/14/"
+# 			},
+# 			{
+# 				"name": "fairy",
+# 				"url": "https://pokeapi.co/api/v2/type/18/"
+# 			}
+# 		],
+# 		"double_damage_to": [
+# 			{
+# 				"name": "normal",
+# 				"url": "https://pokeapi.co/api/v2/type/1/"
+# 			},
+# 			{
+# 				"name": "rock",
+# 				"url": "https://pokeapi.co/api/v2/type/6/"
+# 			},
+# 			{
+# 				"name": "steel",
+# 				"url": "https://pokeapi.co/api/v2/type/9/"
+# 			},
+# 			{
+# 				"name": "ice",
+# 				"url": "https://pokeapi.co/api/v2/type/15/"
+# 			},
+# 			{
+# 				"name": "dark",
+# 				"url": "https://pokeapi.co/api/v2/type/17/"
+# 			}
+# 		],
+# 		"half_damage_from": [
+# 			{
+# 				"name": "rock",
+# 				"url": "https://pokeapi.co/api/v2/type/6/"
+# 			},
+# 			{
+# 				"name": "bug",
+# 				"url": "https://pokeapi.co/api/v2/type/7/"
+# 			},
+# 			{
+# 				"name": "dark",
+# 				"url": "https://pokeapi.co/api/v2/type/17/"
+# 			}
+# 		],
+# 		"half_damage_to": [
+# 			{
+# 				"name": "flying",
+# 				"url": "https://pokeapi.co/api/v2/type/3/"
+# 			},
+# 			{
+# 				"name": "poison",
+# 				"url": "https://pokeapi.co/api/v2/type/4/"
+# 			},
+# 			{
+# 				"name": "bug",
+# 				"url": "https://pokeapi.co/api/v2/type/7/"
+# 			},
+# 			{
+# 				"name": "psychic",
+# 				"url": "https://pokeapi.co/api/v2/type/14/"
+# 			},
+# 			{
+# 				"name": "fairy",
+# 				"url": "https://pokeapi.co/api/v2/type/18/"
+# 			}
+# 		],
+# 		"no_damage_from": [],
+# 		"no_damage_to": [
+# 			{
+# 				"name": "ghost",
+# 				"url": "https://pokeapi.co/api/v2/type/8/"
+# 			}
+# 		]
+# 	},}
 class Pokemon():
     def __init__(self) -> None:
         super().__init__()
@@ -108,18 +108,16 @@ class Pokemon():
         cache = self.cache
         cache.append({url:{f"Object {self.pokeTYPE}": "Object Data of Pokemon Type"}})
         return 
-        # response = requests.get(url)
-        # return response.text
 
-    def get_article(self):
-					if self.pokeTYPE == None:	
-						print("Do this")
-						self.pokeTYPE = input()
-						self.cache.append({self.pokeTYPE: response})
-					else:
-						self.cache.append({self.pokeTYPE: response})
+    # def get_article(self):
+	# 				if self.pokeTYPE == None:	
+	# 					print("Do this")
+	# 					self.pokeTYPE = input()
+	# 					self.cache.append({self.pokeTYPE: response})
+	# 				else:
+	# 					self.cache.append({self.pokeTYPE: response})
 					
-					return
+	# 				return
 
 
 # pokeURL = f"https://pokeapi.co/api/v2/type/{pokeType}"
---
+++ type_getter.py
@@ -0,0 +1,9 @@
+import requests 
+
+class Pokemon:
+    def __init__(self):
+        self.cache = list()
+        self.damageType = None
+    
+    def get_damage_data(): 
+        return
