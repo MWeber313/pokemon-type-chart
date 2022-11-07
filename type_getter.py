727648e822efc8937f7d97db9d31441d9befd3b7
MWeberLambdaweb19 <mack.webb37@gmail.com>
2022-11-07 12:24:40 -0800

    Working request getter, working on configuration to display just damage data

--- type_getter.py
+++ type_getter.py
@@ -5,5 +5,22 @@ class Pokemon:
         self.cache = list()
         self.damageType = None
     
-    def get_damage_data(): 
+    def set_damage_type(self):
+        newType = input()
+        self.damageType = newType
         return
+
+    def get_damage_data(self): 
+        response = requests.get("https://pokeapi.co/api/v2/type/1")
+        self.cache.append({self.damageType:response.text})
+        return
+    
+    def show_damage_data(self):
+        for i in self.cache:
+            print(i)
+        return
+    
+programInstance = Pokemon()
+programInstance.set_damage_type()
+programInstance.get_damage_data()
+programInstance.show_damage_data()
