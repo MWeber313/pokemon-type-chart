2a2c0042c470310e02d794b955732597428dd3da
MWeberLambdaweb19 <mack.webb37@gmail.com>
2022-11-08 15:08:45 -0800

    Working on conditional in damage retrieval function

--- type_getter.py
+++ type_getter.py
@@ -6,16 +6,23 @@ class Pokemon:
         self.damageType = None
     
     def set_damage_type(self):
-        newType = input()
+        newType = input("Please input the id # of the damage type: ")
         self.damageType = newType
         return
 
-    def get_damage_data(self): 
-        response = requests.get("https://pokeapi.co/api/v2/type/1")
-        self.cache.append({self.damageType:response.text})
+    def get_damage_data(self):
+        if self.damageType not in self.cache: 
+            response = requests.get(f"https://pokeapi.co/api/v2/type/{self.damageType}")
+            self.cache.append({self.damageType:response.text})
+            self.show_damage_data()
+        else:
+            self.show_damage_data()
+            print(len(self.cache))
         return
     
     def show_damage_data(self):
+        print(f"This is the cache: {self.cache}")
+        print("This is every item in the cache")
         for i in self.cache:
             print(i)
         return
@@ -24,3 +31,9 @@ programInstance = Pokemon()
 programInstance.set_damage_type()
 programInstance.get_damage_data()
 programInstance.show_damage_data()
+programInstance.set_damage_type()
+programInstance.get_damage_data()
+programInstance.show_damage_data()
+# this should NOT append a new item to the cache
+programInstance.set_damage_type()
+programInstance.get_damage_data()
